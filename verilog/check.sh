6b807f6e5d5aa0e7cfca1a3a485eb2b4c3ca5c53
makaimann <makaim@stanford.edu>
2018-01-12 16:13:11 -0800

    Replace embedded constraints with .smtc file

--- verilog/check.sh
+++ verilog/check.sh
@@ -2,4 +2,4 @@
 
 yosys -q -p 'read_verilog -sv -formal data_integrity_scoreboard.sv; hierarchy -check; prep -top DataIntegritySB; proc; opt; check -assert; write_smt2 -wires disb.smt2'
 
-yosys-smtbmc -s cvc4 --unroll --dump-vcd disb.vcd disb.smt2
+yosys-smtbmc -s cvc4 --unroll --smtc data_integrity.smtc --dump-vcd disb.vcd disb.smt2
