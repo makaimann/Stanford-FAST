f9cf88ef851fc9fe3429d982532e273cc87cb35c
makaimann <makaim@stanford.edu>
2018-01-26 16:13:22 -0800

    Fix a bunch of bugs in Verilog implementations

--- verilog/utils.sv
+++ verilog/utils.sv
@@ -3,12 +3,13 @@
 module FF(clk, en, D, Q);
   parameter WIDTH = 1;
   parameter INIT  = 0;
- 
+
   input wire                clk;
   input wire                en;
   input wire   [WIDTH-1:0]  D;
+
   output reg   [WIDTH-1:0]  Q = INIT;
-  
+
   always @ (posedge clk) begin
     if(en) begin
       Q <= D;
