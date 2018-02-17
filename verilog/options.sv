042ecc30751ee76f2015861a8f55c3ef4dcfd3c3
makaimann <makaim@stanford.edu>
2018-02-16 18:32:00 -0800

    Add options.sv

---
+++ verilog/options.sv
@@ -0,0 +1,13 @@
+`define OPTIONS
+
+// Data Integrity Scoreboard Options
+//`define SB_SANITY
+//`define EMBED
+`define ARBITER
+
+// FIFO Options
+//`define SANITY
+`define ARRAY
+
+// DWRR Arbiter Options
+//`define COMB_UPDATE
