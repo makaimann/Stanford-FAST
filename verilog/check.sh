655bde510347873c3670d727131f1da6d6859b17
makaimann <makaim@stanford.edu>
2018-02-20 19:28:36 -0800

    Add memory_collect pass to check.sh

--- verilog/check.sh
+++ verilog/check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yosys -q -p 'read_verilog -sv -formal data_integrity_scoreboard.sv; hierarchy -check; prep -top Scoreboard; proc; opt; check -assert; write_smt2 -wires disb.smt2'
+yosys -q -p 'read_verilog -sv -formal data_integrity_scoreboard.sv; hierarchy -check; proc; memory_collect; opt; prep -top Scoreboard; check -assert; write_smt2 -wires disb.smt2'
 
 yosys-smtbmc -s $1 -t 30 --unroll --dump-smt2 named_unrolled.smt2 --smtc data_integrity.smtc --dump-vcd disb.vcd disb.smt2
 # For counter examples -- will file issue or make pull request for Yosys fix soon
