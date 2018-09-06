0ba0278250b97f33dd2f593561625ad8ecea1e59
makaimann <makaim@stanford.edu>
2018-09-06 09:34:37 -0700

    Add convenience script for CVC4

---
+++ get_cvc4.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+NOTICE="
+=====================================================================================\n
+This is a convenience script meant to aid in setting up CVC4 with the best configuration\n
+for hardware model checking. Do not rely on this script in any way, and review\n
+the CVC4 license before use: https://github.com/CVC4/CVC4/blob/master/COPYING\n
+=====================================================================================\n\n"
+
+echo -e $NOTICE
+
+git clone https://github.com/CVC4/CVC4.git
+cd CVC4
+
+echo "Getting ANTLR, CaDiCaL, and cryptominisat"
+./contrib/get-antlr-3.4 && ./contrib/get-cadical && ./contrib/get-cryptominisat
+
+echo "Setting up build system"
+./autogen.sh
+
+echo "Configuring CVC4 with best options for hardware verification"
+./configure production-staticbinary --with-cadical --with-cryptominisat
+
+make -j$(nproc)
+
+cd ../
+
+echo ""
+echo "The static binary is available at ./CVC4/builds/bin/"
