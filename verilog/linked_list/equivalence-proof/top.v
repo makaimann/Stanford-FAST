38213a03d1e00df304457a76e72754df77287cce
Makai Mann <makaim@stanford.edu>
2019-04-19 14:29:26 -0700

    Add history variables

--- verilog/linked_list/equivalence-proof/top.v
+++ verilog/linked_list/equivalence-proof/top.v
@@ -3,18 +3,23 @@
 `endif
 
 module top(clk, rst, push, pop, push_sel, pop_sel, data_in,
+           // dummy inputs
+           free_head_ptr, idx,
+           // outputs
            empty, full, data_out,
            srempty, srfull, srdata_out);
    parameter WIDTH=8,
              DEPTH=4,
              NUM_FIFOS=2,
              FIFO_SEL=0,
+             PTR_WIDTH=$clog2(DEPTH),
              SEL_WIDTH=$clog2(NUM_FIFOS),
              ADDR_WIDTH=$clog2(NUM_FIFOS+1);
 
    input                           clk, rst, push, pop;
    input [SEL_WIDTH-1:0]           push_sel, pop_sel;
    input [WIDTH-1:0]               data_in;
+   input [PTR_WIDTH-1:0]           free_head_ptr, idx; // will be constrained by CoSA
    output                          full;
    output [NUM_FIFOS-1:0]          empty;
    output [WIDTH-1:0]              data_out;
@@ -71,5 +76,26 @@ module top(clk, rst, push, pop, push_sel, pop_sel, data_in,
        .empty(srempty),
        .full(srfull),
        .data_out(srdata_out));
+
+   // history variables
+   (* keep *)
+   reg [PTR_WIDTH-1:0] sr_to_ll [DEPTH-1:0];
+   (* keep *)
+   reg [PTR_WIDTH-1:0] ll_to_sr [DEPTH-1:0];
+
+   (* keep *)
+   wire [PTR_WIDTH-1:0] sr_result;
+   (* keep *)
+   wire [PTR_WIDTH-1:0] ll_result;
+
+   always @(posedge clk) begin
+      if (srpush) begin
+         sr_to_ll[idx] <= free_head_ptr;
+         ll_to_sr[free_head_ptr] <= idx;
+      end
+   end
+
+   assign sr_result = sr_to_ll[idx];
+   assign ll_result = ll_to_sr[free_head_ptr];
 
 endmodule // top
