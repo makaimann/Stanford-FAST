f8c41c5a0980b835cf65a887c29b4fd214ca1399
Makai Mann <makaim@stanford.edu>
2019-04-22 11:15:05 -0700

    Add ll_to_sr updates (required some non-trivial lemmas)

--- verilog/linked_list/equivalence-proof/top.v
+++ verilog/linked_list/equivalence-proof/top.v
@@ -4,7 +4,7 @@
 
 module top(clk, rst, push, pop, push_sel, pop_sel, data_in,
            // dummy inputs
-           free_tail_ptr, next_head, idx,
+           free_tail_ptr, head0, next_head0, idx,
            // outputs
            empty, full, data_out,
            srempty, srfull, srdata_out);
@@ -19,7 +19,7 @@ module top(clk, rst, push, pop, push_sel, pop_sel, data_in,
    input                           clk, rst, push, pop;
    input [SEL_WIDTH-1:0]           push_sel, pop_sel;
    input [WIDTH-1:0]               data_in;
-   input [PTR_WIDTH-1:0]           free_tail_ptr, next_head, idx; // will be constrained by CoSA
+   input [PTR_WIDTH-1:0]           free_tail_ptr, head0, next_head0, idx; // will be constrained by CoSA
    output                          full;
    output [NUM_FIFOS-1:0]          empty;
    output [WIDTH-1:0]              data_out;
@@ -96,22 +96,22 @@ module top(clk, rst, push, pop, push_sel, pop_sel, data_in,
    //       ll_to_sr[free_tail_ptr] <= idx;
    //    end
    //    if (srpop | (srpush & srempty)) begin
-   //       sr_to_ll[0] <= next_head;
-   //       ll_to_sr[next_head] <= 0;
+   //       sr_to_ll[0] <= next_head0;
+   //       ll_to_sr[next_head0] <= 0;
    //    end
    // end
 
    // always @(posedge clk) begin
    //    if (srpush) begin
    //       if (srempty | srpop) begin
-   //          sr_to_ll[0] <= next_head;
+   //          sr_to_ll[0] <= next_head0;
    //       end
    //       else begin
    //          sr_to_ll[idx] <= free_tail_ptr;
    //       end
    //    end
    //    else if (srpop) begin
-   //       sr_to_ll[0] <= next_head;
+   //       sr_to_ll[0] <= next_head0;
    //    end
    // end // always @ (posedge clk)
 
@@ -120,11 +120,15 @@ module top(clk, rst, push, pop, push_sel, pop_sel, data_in,
       // If we only have the first case, this works if we just care about the head
       //   something about tracking the tail as well makes this more complicated
       if (srpop | (srpush & srempty)) begin
-         sr_to_ll[0] <= next_head;
+         sr_to_ll[0] <= next_head0;
+         ll_to_sr[next_head0] <= 0;
       end
-      // condition on idx is redundant but helps the inductive proof
-      if (srpush & !srempty & (idx != 0)) begin
+      // conditions on idx and free_tail_ptr are redundant but helps the inductive proof
+      // they're basically lemmas built into the history var update
+      // TODO : see if these extra conditions are necessary once we have the full mapping?
+      if (srpush & !srempty & (idx != 0) & (free_tail_ptr != head0) & (free_tail_ptr != next_head0)) begin
          sr_to_ll[idx] <= free_tail_ptr;
+         ll_to_sr[free_tail_ptr] <= idx;
       end
    end
 
