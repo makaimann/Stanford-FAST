1d5014a355ae501c6a70ebb24494b43a211419b6
Makai Mann <makaim@stanford.edu>
2019-05-19 10:16:08 -0700

    Create fake config in por file and instantiate a bmc object

--- verilog/arbitrated_fifos/por.py
+++ verilog/arbitrated_fifos/por.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from cosa.encoders.verilog_yosys import VerilogYosysBtorParser
+from cosa.analyzers.mcsolver import BMCSolver
 
 from pysmt.shortcuts import BOOL, BV, EqualsOrIff
 
@@ -10,9 +11,9 @@ from pathlib import Path
 # hacky -- creating config here
 # should have an api command to get a generic config object from CoSA
 
-btor_config = namedtuple('btor_config', 'abstract_clock opt_circuit no_arrays symbolic_init')
+btor_config = namedtuple('btor_config', 'abstract_clock opt_circuit no_arrays symbolic_init strategy skip_solving smt2_tracing solver_name incremental solver_options')
 
-def reduced_instruction_set(hts, actions):
+def reduced_instruction_set(hts, config, actions):
     for a in actions:
         assert a.get_type() == BOOL
 
@@ -20,13 +21,21 @@ def reduced_instruction_set(hts, actions):
     print("Got HTS:", hts)
     print("\tWith actions={}".format(actions))
 
+    bmc = BMCSolver(hts, config)
+
 def main():
     parser = VerilogYosysBtorParser()
     fifo_source = Path("./fifo.sv")
     config = btor_config(abstract_clock=True,
                          opt_circuit=False,
                          no_arrays=False,
-                         symbolic_init=False)
+                         symbolic_init=False,
+                         strategy='FWD',
+                         skip_solving=False,
+                         smt2_tracing=None,
+                         solver_name='btor',
+                         incremental=True,
+                         solver_options=None)
 
     hts, inv_a, ltl_a = parser.parse_file(fifo_source, config, flags=['fifo'])
 
@@ -39,7 +48,7 @@ def main():
 
     actions = [EqualsOrIff(push, BV(1, 1)), EqualsOrIff(pop, BV(1, 1))]
 
-    reduced_instruction_set(hts, actions)
+    reduced_instruction_set(hts, config, actions)
 
 if __name__ == "__main__":
     main()
