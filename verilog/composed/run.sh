391312d550c38fa504d3a1e4c788fe5fc24b3ed5
Makai Mann <makaim@stanford.edu>
2019-07-01 12:58:28 -0700

    Minor fix to run script

--- verilog/composed/run.sh
+++ verilog/composed/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if ["$#" -eq 0] || [ "$1" = "composed" ]; then
+if [ "$#" -eq 0 ] || [ "$1" = "composed" ]; then
     /home/makaim/.local/bin/run_abc -t top -e pdr -s='-D=../arbitrated_fifos/' -s ../arbitrated_fifos/fifo.sv -s ../arbitrated_fifos/MagicPacketTracker.v -s ./SimpleScoreboard.sv -s ./top.v
 elif [ "$1" = "abstract" ]; then
     /home/makaim/.local/bin/run_abc -t top -e pdr -s='-D=../arbitrated_fifos/' -s ../arbitrated_fifos/fifo.sv -s ../arbitrated_fifos/MagicPacketTracker.v -s ./SimpleScoreboard.sv -s ./abstract_top.v
