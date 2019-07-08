116cd29265ab1be06acfc447972ee0c245ba34ae
Makai Mann <makaim@stanford.edu>
2019-07-08 14:04:30 -0700

    Fix other run_abc calls

--- verilog/composed/run.sh
+++ verilog/composed/run.sh
@@ -3,7 +3,7 @@
 if ["$#" -eq 0] || [ "$1" = "composed" ]; then
     ../run_abc.py -t top -e pdr -s='-D=../arbitrated_fifos/' -s ../arbitrated_fifos/fifo.sv -s ../arbitrated_fifos/MagicPacketTracker.v -s ./SimpleScoreboard.sv -s ./top.v
 elif [ "$1" = "abstract" ]; then
-    /home/makaim/.local/bin/run_abc -t top -e pdr -s='-D=../arbitrated_fifos/' -s ../arbitrated_fifos/fifo.sv -s ../arbitrated_fifos/MagicPacketTracker.v -s ./SimpleScoreboard.sv -s ./abstract_top.v
+    ../run_abc.py -t top -e pdr -s='-D=../arbitrated_fifos/' -s ../arbitrated_fifos/fifo.sv -s ../arbitrated_fifos/MagicPacketTracker.v -s ./SimpleScoreboard.sv -s ./abstract_top.v
 elif [ "$1" = "fifo" ]; then
-    /home/makaim/.local/bin/run_abc -t fifo_top -e pdr -s='-D=../arbitrated_fifos/' -s ../arbitrated_fifos/fifo.sv -s ../arbitrated_fifos/MagicPacketTracker.v -s ./SimpleScoreboard.sv -s ./fifo_top.sv
+    ../run_abc.py -t fifo_top -e pdr -s='-D=../arbitrated_fifos/' -s ../arbitrated_fifos/fifo.sv -s ../arbitrated_fifos/MagicPacketTracker.v -s ./SimpleScoreboard.sv -s ./fifo_top.sv
 fi
